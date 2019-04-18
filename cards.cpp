8b13eb1435f78bf4865221137f15f21b65aa5f15
kadomak <47510056+kadomak@users.noreply.github.com>
2019-04-18 00:06:37 -0400

    Create cards.cpp

---
+++ cards.cpp
@@ -0,0 +1,9 @@
+#include "cards.h"
+
+//defining the total number or suits and ranks
+const int cards :: SUITMAX  = 4;
+const int cards :: RANKMAX = 13;
+
+//an array of the suits and ranks
+const string cards :: suitList[] = {"♠","♥", "♣", "♦" };
+const string cards :: rankList[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10","J", "Q", "K", "A"};
