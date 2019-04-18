18ea97de253fef36743e5ad42c4a7ef64dd98a4e
kadomak <47510056+kadomak@users.noreply.github.com>
2019-04-18 00:06:13 -0400

    Create cards.h

---
+++ cards.h
@@ -0,0 +1,18 @@
+#ifndef CARDS_H
+#define CARDS_H 
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+class cards
+{
+    protected:
+    static const int SUITMAX; //length of arrays
+    static const int RANKMAX;
+    static const string suitList[]; //holds the ranks and suits
+    static const string rankList[]; 
+    
+};
+
+#endif
